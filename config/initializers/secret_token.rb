696b4883947a7986437743376265097985a8be45
dreyepat <titz@gmx.ch>
2013-03-05 14:45:05 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wp13::Application.config.secret_token = 'cb6a475263f9b5ff99c7ae1b8a1a60daf245ce4ad92d37b2e3c7f95db8cd15cfa82f00360ac6b022320acae621291baf77d9cbbe8f1c2b9bf824e1d398fc285f'
