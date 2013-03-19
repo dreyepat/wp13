c6597c2d456c5b45a9dbf7a0332fc716900204fd
dreyepat <titz@gmx.ch>
2013-03-19 15:49:43 +0100

    Validierungen angepasst

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,12 @@
 class Product < ActiveRecord::Base
-  attr_accessible :available, :description, :image_url, :price, :title
+
+	attr_accessible :available, :description, :image_url, :price, :title
+	validates_presence_of :title, :description, :price
+	validates_numericality_of :price, numericality: {greater_than_or_equal_to: 0.05}
+	validates :title, uniqueness: true
+	validates :image_url, allow_blank: true, format: {
+	with: %r{\.(gif|jpg|png)\Z}i,
+	message: 'must be a URL for GIF, JPG or PNG image.'
+	}
   
-  validates_presence_of :title, :description, :price
-  validates_numericality_of :price
 end
