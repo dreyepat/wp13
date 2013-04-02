861a314559e20449552d42851627501ccda5b747
dreyepat <titz@gmx.ch>
2013-04-02 15:19:27 +0200

    Test und Controller

--- test/functional/products_controller_test.rb
+++ test/functional/products_controller_test.rb
@@ -14,6 +14,7 @@ class ProductsControllerTest < ActionController::TestCase
   test "should get index" do
     get :index
     assert_response :success
+	assert_select '#main .entry', 3 # see fixtures
     assert_not_nil assigns(:products)
   end
 
