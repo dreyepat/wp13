71f26b2b9122be2df394fd281d6c45f138f26ac0
dreyepat <titz@gmx.ch>
2013-04-09 15:23:13 +0200

    Cart erstellt

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,7 @@
 class Product < ActiveRecord::Base
+	has_many :line_items
+	
+	before_destroy :ensure_not_referenced_by_any_line_item
 
 	attr_accessible :available, :description, :image_url, :price, :title
 	validates_presence_of :title, :description, :price
@@ -8,5 +11,14 @@ class Product < ActiveRecord::Base
 	with: %r{\.(gif|jpg|png)\Z}i,
 	message: 'must be a URL for GIF, JPG or PNG image.'
 	}
-  
+	
+	private
+	def ensure_not_referenced_by_any_line_item
+		if line_items.empty?
+			return true
+		else
+			errors.add(:base, 'Line Items present')
+			return false
+		end
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Wp13::Application.routes.draw do
+  resources :line_items
+
+  resources :carts
+
   # get "store/index"
   resources :products
 
