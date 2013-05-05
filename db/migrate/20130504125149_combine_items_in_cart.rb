ddcd4b883e4b2c213b3d7697485c904f5ebef308
dreyepat <titz@gmx.ch>
2013-05-05 14:06:33 +0200

    ein weiterer Versuch

--- db/migrate/20130504125149_combine_items_in_cart.rb
+++ db/migrate/20130504125149_combine_items_in_cart.rb
@@ -8,7 +8,7 @@ class CombineItemsInCart < ActiveRecord::Migration
 
       sums.each do |product_id, quantity|
         if quantity > 1
-          # remove individual items
+          # remove individual items 
           cart.line_items.where(product_id: product_id).delete_all
 
           # replace with a single item
--- db/schema.rb
+++ db/schema.rb
@@ -18,6 +18,14 @@ ActiveRecord::Schema.define(:version => 20130504125149) do
     t.datetime "updated_at", :null => false
   end
 
+  create_table "line_items", :force => true do |t|
+    t.integer  "product_id"
+    t.integer  "cart_id"
+    t.datetime "created_at",                :null => false
+    t.datetime "updated_at",                :null => false
+    t.integer  "quantity",   :default => 1
+  end
+
   create_table "products", :force => true do |t|
     t.string   "title"
     t.text     "description"
