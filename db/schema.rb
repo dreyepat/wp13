fadf753eb8e08dd591f7d87795bdb4d943556d78
dreyepat <titz@gmx.ch>
2013-05-05 13:24:18 +0200

    weitere Anpassung

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130504124622) do
+ActiveRecord::Schema.define(:version => 20130504125149) do
 
   create_table "carts", :force => true do |t|
     t.datetime "created_at", :null => false
